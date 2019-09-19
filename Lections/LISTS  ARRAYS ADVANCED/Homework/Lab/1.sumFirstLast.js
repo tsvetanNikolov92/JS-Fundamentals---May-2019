30dd5224ff76bf4f8e65b924dd194559d55338c6
tsvetanNikolov92 <cvetan.nikolov1@abv.bg>
2019-09-19 04:50:54 +0300

    Updated

--- Lections/LISTS  ARRAYS ADVANCED/Homework/Lab/1.sumFirstLast.js
+++
@@ -1,6 +0,0 @@
-function sumFirstLast(arrOfString) {
-    let firstEl = Number(arrOfString[0]);
-    let lastEl = Number(arrOfString[arrOfString.length - 1]);
-
-    console.log(firstEl + lastEl);
-}
